b34dddc6ec12891b78e9e3beecb2fd68fa5c7212
Vitoria Galvao <vitoriaggalvao19@gmail.com>
2021-09-09 20:12:41 -0300

    atualizacao

--- src/data/database.js
+++ src/data/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-const MONGO_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/Beer-db" 
+const MONGO_URL = process.env.MONGODB_URI || "mongodb://localhost:27017/Beer-db" 
 
 const connect = () => {
     mongoose.connect(MONGO_URL, {
