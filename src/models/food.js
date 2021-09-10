df829f7cd368f988e367908a7b3a7c840348de75
Vitoria Galvao <vitoriaggalvao19@gmail.com>
2021-09-10 20:33:51 -0300

    atualizacao food

--- src/models/food.js
+++ src/models/food.js
@@ -1,26 +1,26 @@
 const mongoose = require('mongoose')
 
 const foodSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
+  
   name: {
     type: String,
     required: true,
     ref: 'beer'
  },
- _id: mongoose.Schema.Types.ObjectId,
+ 
   name: {
     type: String,
     required: true,
     ref: 'beer'
   },
-  _id: mongoose.Schema.Types.ObjectId,
+ 
   name: {
     type: String,
     required: true,
     ref: 'beer'
  
   },
-  _id: mongoose.Schema.Types.ObjectId,
+ 
   name: {
     type: String,
     required: true,
